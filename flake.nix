25db41ecac804d7d020065a6689c48e74fd562c8
spicadust <spicastre@gmail.com>
2025-02-19 23:54:59 +0000

    Setup uv environment

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Python environment with uv";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          python312
+
+          (python312.withPackages (ps:
+            with ps; [
+              uv
+            ]))
+        ];
+      };
+    });
+}
